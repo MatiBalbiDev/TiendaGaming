dfdca76dd2d6cc293949d9de61735d39b2050d06
MatiBalbiDev <matiasbalbi96@gmail.com>
2022-01-23 18:14:03 -0300

    agregando archivos

---
+++ TiendaGaming/src/main/java/com/tiendaGaming/TiendaGamingApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaGaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGamingApplication.class, args);
+	}
+
+}
---
+++ TiendaGaming/src/test/java/com/tiendaGaming/TiendaGamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaGaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
