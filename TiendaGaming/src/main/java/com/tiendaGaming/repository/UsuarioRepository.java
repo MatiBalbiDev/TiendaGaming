892d3dc74f5517c261ddbba83b07da237acbc2a2
MatiBalbiDev <matiasbalbi96@gmail.com>
2022-02-16 16:39:38 -0300

    Cambios en la firma de los metodos y otros agregados.

--- TiendaGaming/src/main/java/com/tiendaGaming/repository/UsuarioRepository.java
+++ TiendaGaming/src/main/java/com/tiendaGaming/repository/UsuarioRepository.java
@@ -1,12 +1,17 @@
 package com.tiendaGaming.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tiendaGaming.model.Usuario;
 
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	Usuario findByUsername(String nombre);
+	Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+	Boolean existsByNombreUsuario(String nombreUsuario);
+	Boolean existsByEmail(String email);
 	
 }
